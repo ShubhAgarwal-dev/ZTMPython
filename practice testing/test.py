627cdc658db1bf4daf9b85b2ec976b0294ebf734
Shubh Agarwal <sazorinos1111@gmail.com>
2021-11-19 17:24:45 +0530

    updated it

--- practice testing/test.py
+++ practice testing/test.py
@@ -3,6 +3,9 @@ import main
 
 
 class TestMain(unittest.TestCase):
+    def setUp(self) -> None:
+        print("About to Run test")
+
     def test_add_5(self):
         test_param = 10
         result = main.add_5(test_param)
