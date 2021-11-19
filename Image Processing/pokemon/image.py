d3ee4b85982c174a5024472da37b7ce0fbe4a717
Shubh Agarwal <sazorinos1111@gmail.com>
2021-11-19 22:49:47 +0530

    halting it for some time

--- Image Processing/pokemon/image.py
+++ Image Processing/pokemon/image.py
@@ -3,7 +3,9 @@ from PIL import Image, ImageFilter
 
 img = Image.open(r'.\Image Processing\pokemon\imgs\pikachu.jpg')
 filtered_img = img.filter(ImageFilter.BLUR)
-filtered_img2 = img.filter(ImageFilter.BoxBlur(radius=3))
+filtered_img2 = img.filter(ImageFilter.BoxBlur(radius=1.9))
 
 filtered_img.save(r".\Image Processing\pokemon\processed\blur.png", 'png')
 filtered_img2.save(r"Image Processing\pokemon\processed\boxblur.png", 'png')
+filtered_img3 = filtered_img2.rotate(angle=90.0)
+filtered_img3.save(r"Image Processing\pokemon\processed\rotate.png", 'png')
