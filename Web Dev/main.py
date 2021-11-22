7de65766a70244746fc5236962f2954137f054ae
Shubh Agarwal <sazorinos1111@gmail.com>
2021-11-22 21:58:42 +0530

    Now it is fully functional with some trickery

--- Web Dev/main.py
+++ Web Dev/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, send_file, redirect, url_for
 
 app = Flask(__name__)
 
@@ -18,24 +18,24 @@ def blog():
 
 @app.route('/cafe')
 def cafe():
-    return render_template('index-1.html')
+    return render_template('index.html')
 
 
 # @app.route('/css/<file>')
 # def css_route(file):
 #     return render_template('css/%s' % file)
-# Dome wrong way
+# # Done wrong way
 
 
 @app.route('/images/<file>')
 def images_route(file):
-    return send_file('templates/images/%s' % file)
+    return send_file('static/images/%s' % file)
 
 
-# @app.route('/js/<file>')
-# def js_route(file):
-#     return render_template('js/%s' % file)
-# Dome Wrong way
+@app.route('/js/<file>')
+def js_route(file):
+    return render_template('/js/%s' % file)
+# # Done Wrong way
 
 
 @app.route('/bat/<file>')
@@ -43,5 +43,30 @@ def bat_route(file):
     return render_template('bat/%s' % file)
 
 
-if '__name__' == '__main__':
-    app.run()
+@app.route('/index.html')
+def index_route():
+    return render_template('index.html')
+
+
+@app.route('/index-1.html')
+def index1_route():
+    return render_template('index-1.html')
+
+
+@app.route('/index-2.html')
+def index2_route():
+    return render_template('index-2.html')
+
+
+@app.route('/index-3.html')
+def index3_route():
+    return render_template('index-3.html')
+
+
+@app.route('/index-4.html')
+def index4_route():
+    return render_template('index-4.html')
+
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=5000, debug=True)
