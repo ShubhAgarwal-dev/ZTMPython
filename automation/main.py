c673c115641e322a2db6da5d680aab0cb2f1b39f
Shubh Agarwal <sazorinos1111@gmail.com>
2021-11-24 00:25:38 +0530

    closing it

--- automation/main.py
+++ automation/main.py
@@ -1,7 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver import firefox
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox import options
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 
@@ -22,11 +20,20 @@ assert 'Selenium Easy Demo' in firefox_instance.title
 # We can also use print and it will not error out
 
 button_element = firefox_instance.find_element_by_class_name("btn-default")
-print(button_element.get_attribute('innerHTML'))
+# we can also use find_element_by_css_selector("#get-input > .btn") Note: # tells that it is ID and . tells us that is css class
+# print(button_element.get_attribute('innerHTML'))
 
 assert 'Show Message' in firefox_instance.page_source
 
 user_message = firefox_instance.find_element_by_id("user-message")
 user_message.clear()
-message = "Hello we can do it."
+message = "Hello we can make python do it."
 user_message.send_keys(message)
+button_element.click()
+output_message = firefox_instance.find_element_by_id("display")
+
+assert message in output_message.get_attribute('innerHTML')
+
+firefox_instance.close()
+firefox_instance.close()
+# Sometimes you are required to close it twice because it would not work for first time
