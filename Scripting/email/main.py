38e2b0bc98799d1fa394f506878792b23d09625f
Shubh Agarwal <sazorinos1111@gmail.com>
2021-12-13 16:04:15 +0530

    making use of html to send dynamic email

--- Scripting/email/main.py
+++ Scripting/email/main.py
@@ -10,6 +10,23 @@ email['subject'] = 'Hello, you are selected to do automation'
 email.set_content(
     'I am very happy that I, Shubh Agarwal could be doing automation now.')
 
+'''
+# to send the email content from html:
+
+from string import Template
+from pathlin import path
+
+html = Template(Path('index.html).read_text())
+# you can have variables in the HTML file like($name, $a and many more) 
+    # any you can substitute them here.
+# Suppose the html had variable $name in the <body>...$name, $age </body>
+
+
+
+and replace email.set_content with
+email.set_content(html.substitute({'name': 'Shubh', 'age': '18'}), 'html')
+'''
+
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
