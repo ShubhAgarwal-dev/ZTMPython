607fc7e16fbd386d30d01eca8c74e00fcaa7c552
Shubh Agarwal <sazorinos1111@gmail.com>
2021-12-28 18:30:01 +0530

    Made it a OOP

--- Database/main.py
+++ Database/main.py
@@ -1,38 +1,38 @@
 import sqlite3
 
 
-def create_table():
-    connection = sqlite3.connect('cinema.db')
-    connection.execute("""
-        CREATE TABLE "Seat"(
-            "seat_id" TEXT,
-            "taken" INTEGER,
-            "price" REAL
-        );
+class DatabaseManipulation():
+    def __init__(self, database_path='cinema.db') -> None:
+        self._filepath = database_path
+        self.create_table()
 
-        """)
-    connection.commit()
-    connection.close()
+    def create_table(self):
+        connection = sqlite3.connect(self._filepath)
+        connection.execute("""
+            CREATE TABLE "Seat"(
+                "seat_id" TEXT,
+                "taken" INTEGER,
+                "price" REAL
+            );
 
+            """)
+        connection.commit()
+        connection.close()
 
-def add_record(seat_id: str, tanken: bool | int, price: float):
-    connection = sqlite3.connect('cinema.db')
-    connection.execute("""
-        INSERT INTO "Seat"("seat_id", "taken", "price") VALUES (?, ?, ?)
-        """, [seat_id, int(tanken), price])
-    connection.commit()
-    connection.close()
+    def add_record(self, seat_id: str, tanken: bool | int, price: float):
+        connection = sqlite3.connect(self._filepath)
+        connection.execute("""
+            INSERT INTO "Seat"("seat_id", "taken", "price") VALUES (?, ?, ?)
+            """, [seat_id, int(tanken), price])
+        connection.commit()
+        connection.close()
 
-
-def select_all():
-    connection = sqlite3.connect('cinema.db')
-    cursor = connection.cursor()
-    cursor.execute("""
-        SELECT * FROM "Seat"
-        """)
-    result = cursor.fetchall()
-    connection.close
-    return result
-
-
-print(select_all())
+    def select_all(self):
+        connection = sqlite3.connect(self._filepath)
+        cursor = connection.cursor()
+        cursor.execute("""
+            SELECT * FROM "Seat"
+            """)
+        result = cursor.fetchall()
+        connection.close
+        return result
